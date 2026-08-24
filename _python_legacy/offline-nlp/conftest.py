7fad921e6d75d534f07d90e14d25e407d648ea85
0xriteish <rt8552821@gmail.com>
2026-08-24 11:58:00 +0545

    pycache cleared

--- _python_legacy/offline-nlp/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add the offline-nlp root directory to sys.path so that
-# `from src.engine import ...` works correctly when pytest is run
-# from either the offline-nlp directory or the repo root.
-sys.path.insert(0, os.path.dirname(__file__))
